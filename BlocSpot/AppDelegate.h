10689c6b3529a3ffcf8f637756e4d824d72fdc4d
Jonathan Jungemann <jrjungemann@gmail.com>
2014-11-23 10:26:29 -0600

    Initial Commit

---
+++ BlocSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocSpot
+//
+//  Created by Jonathan Jungemann on 11/23/14.
+//  Copyright (c) 2014 Jon Jungemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
